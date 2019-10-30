9ab54c1fca46b0b44d0ae1932d2b1604e8cd1d8e
Spotlighter <m.schmid@spotlighter.de>
2019-10-30 22:20:01 +0100

    Just added the base

---
+++ resources/frontend/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+
+    <h1>{{msg}}</h1>
+
+    <!--
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    //HelloWorld
+  },
+  data: function(){
+    return {
+      msg: 'Hello World'
+    }
+  }
+}
+</script>
---
+++ resources/js/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Crafter</span>
+        <span class="font-weight-light">Office</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="#"
+        target="_blank"
+      >
+        <span class="mr-2">Nice Dude</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <SideBarDrawer></SideBarDrawer>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld';
+import SideBarDrawer from './components/SideBarDrawer';
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    SideBarDrawer,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
